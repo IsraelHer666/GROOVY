b538a96f4a9b2c02669e65f11c971456626a81f3
IsraelHer666 <bisrael240@gmail.com>
2025-11-06 20:38:19 -0600

    Adding photos

--- js/places.js
+++ js/places.js
@@ -1,8 +1,15 @@
-//Selecting objects
+const button = document.getElementById("nextButton");
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
 
-const button = document.getElementsByClassName("nextButton");
+button.addEventListener("click",(e)=>{
 
-$(button).click(function (e) { 
-    alert("Hola")
-    e.preventDefault();
-});
+    const  collection = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    let number  = (Math.random()).toString();
+    let numberSplitted =  number.split(".",2);
+    console.log(numberSplitted[0])
+   img1.setAttribute("src","../img/places/place"+collection[numberSplitted[0]]);
+   img2.setAttribute("src","../img/places/place");
+//    img3.setAttribute("src","../img/places/places.jpg");
+})
